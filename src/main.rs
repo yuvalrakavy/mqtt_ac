994806e12666c57f94bd898a1c6a73ef719cf794
Yuval Rakavy <yuval@villarakavy.com>
2023-03-05 00:36:09 +0200

    Added command line handling

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,27 @@ mod mqtt_publisher;
 mod mqtt_subscriber;
 mod polling;
 
+use rustop::opts;
 use service::ServiceConfig;
 
 #[tokio::main]
 async fn main() {
-    let config = ServiceConfig {
-        mqtt_broker_address: String::from("control-bz"),
-        controller_name: String::from("BZ"),
-        coolmaster_address: String::from("10.0.1.70"),
-        polling_period: tokio::time::Duration::from_secs(4),
-    };
+    let (args, _) = opts! {
+        synopsis "MQTT Coolmaster (aircondition) Controller";
+        param controller_name:String, desc: "Controller name";
+        param mqtt:String, desc: "MQTT broker to connect";
+        param coolmaster_address:String, desc: "Coolmaster address";
+        opt polling: u16=4, desc: "Polling period (in seconds)";
+    }.parse_or_exit();
 
-    set_logger();
+    env_logger::init();
+
+    let config = ServiceConfig {
+        controller_name: args.controller_name,
+        mqtt_broker_address: args.mqtt,
+        coolmaster_address: args.coolmaster_address,
+        polling_period: tokio::time::Duration::from_secs(args.polling as u64),
+    };
 
     let service = service::Service::new(config);
 
@@ -29,6 +38,6 @@ async fn main() {
     let _ = service.stop().await;
 }
 
-fn set_logger() {
-    let _ = env_logger::builder().is_test(true).filter_level(log::LevelFilter::Debug).filter_module("rumqttc", log::LevelFilter::Off).try_init();
-}
+// fn set_logger() {
+//     let _ = env_logger::builder().is_test(true).filter_level(log::LevelFilter::Debug).filter_module("rumqttc", log::LevelFilter::Off).try_init();
+// }
